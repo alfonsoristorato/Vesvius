8c1442c6969ab0d0968334d88517c5064e5bcd4c
alfonsoristorato <alfonsoristorato@gmail.com>
2021-01-31 21:02:17 +0000

    update

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@
             if (target.length) {
                 $("html, body").animate(
                     {
-                        scrollTop: target.offset().top - 70,
+                        scrollTop: target.offset().top -1, //modify offset
                     },
                     1000,
                     "easeInOutExpo"
